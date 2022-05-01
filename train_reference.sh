c8b7ac864c198c9c6d61dcf13999b91e6266940a
Michał Jamry <michal.jamry@yougov.com>
2022-05-01 23:42:08 +0200

    test1 model

--- train_reference.sh
+++ train_reference.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir -p experiments/base
+mkdir -p experiments/reference
+
 python edit_config.py --train_dir /waymo/splitted/train/ --eval_dir /waymo/splitted/val/ --batch_size 2 --checkpoint /app/experiments/pretrained_model/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8/checkpoint/ckpt-0 --label_map /app/project/experiments/label_map.pbtxt
 
-mv pipeline_new.config experiments/base/
+mv pipeline_new.config experiments/reference/
 
-python experiments/model_main_tf2.py --model_dir=experiments/base/ --pipeline_config_path=experiments/base/pipeline_new.config
+python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config
