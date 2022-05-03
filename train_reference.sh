f17488ac0d329b4303e2eb7ccfdffe68c8b57b11
Michał Jamry <michal.jamry@yougov.com>
2022-05-03 22:54:04 +0200

    work

--- train_reference.sh
+++ train_reference.sh
@@ -6,3 +6,4 @@
 #mv pipeline_new.config experiments/reference/
 
 python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config
+python experiments/model_main_tf2.py --model_dir=experiments/reference/ --pipeline_config_path=experiments/reference/pipeline_new.config --checkpoint_dir=experiments/reference/
--- train_test2.sh
+++ train_test2.sh
@@ -1,1 +1,2 @@
 python experiments/model_main_tf2.py --model_dir=experiments/test2/ --pipeline_config_path=experiments/test2/pipeline_new.config
+python experiments/model_main_tf2.py --model_dir=experiments/test2/ --pipeline_config_path=experiments/test2/pipeline_new.config --checkpoint_dir=experiments/test/
